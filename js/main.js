04c01d435edbc233ddc85355043071756b39261c
rahulshinde <rshinde@mica.edu>
2014-06-11 17:25:09 -0400

    updated html and css styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+var fixmeTop = $('.nav-fixed').offset().top; // Get initial position
+$(window).scroll(function() {            // Assign scroll event listener
+    var currentScroll = $(window).scrollTop(); // Get current position
+    if (currentScroll >= fixmeTop) { // Make it fixed if you've scrolled to it
+        $('.nav-fixed').css({
+            position: 'fixed',
+            top: '0',
+            right: '0'
+        });
+    } else {                       // Make it static if you scroll above
+        $('.nav-fixed').css({
+            position: 'static'
+        });
+    }
+});
