dd0be4803dcc7ee2f850cdffde7e8b24f8f9746b
rahulshinde <rshinde@mica.edu>
2014-06-12 03:31:38 -0400

    js changes, minor html updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-var fixmeTop = $('.nav-fixed').offset().top; // Get initial position
-$(window).scroll(function() {            // Assign scroll event listener
-    var currentScroll = $(window).scrollTop(); // Get current position
-    if (currentScroll >= fixmeTop) { // Make it fixed if you've scrolled to it
-        $('.nav-fixed').css({
-            position: 'fixed',
-            bottom: '0',
-            right: '0'
-        });
-    } else {                       // Make it static if you scroll above
-        $('.nav-fixed').css({
-            position: 'static'
-        });
+//waypoints for .nav-up
+$(function(){
+    $('#menu-nav').waypoint(function(direction) {
+        console.log(direction); 
+        $('#nav-up').toggleClass('is-visible');
+    }, {
+        offset: function() {
+            return $(this).height() * -1;
+        }
+    });
+});
+
+
+// smooth scroll js
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
+  });
 });
